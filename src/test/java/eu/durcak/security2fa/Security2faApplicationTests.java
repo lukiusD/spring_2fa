c2c0c121988f8cf0961224a60185550212a0cf0b
Lukáš Ďurčák <lukas@durcak.eu>
2025-01-22 22:25:52 +0100

    test

---
+++ src/test/java/eu/durcak/security2fa/Security2faApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.durcak.security2fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security2faApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
